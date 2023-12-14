1d9f1b2ee4ca585cc020f644db55b9c89f2348c0
SANDMAN <126715601+SANDY-FM-III@users.noreply.github.com>
2023-12-14 18:49:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/UnknownBotz/EasyAdvAutoFilter.git /EasyAdvAutoFilter
+  git clone https://github.com/SANDY-FM-III/SATORU-.git /SATORU-
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EasyAdvAutoFilter
+  git clone $UPSTREAM_REPO /SATORU
 fi
-cd /EasyAdvAutoFilter
+cd /SATORU
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
